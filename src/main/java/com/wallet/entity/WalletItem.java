94c29c5542bf5e899c0c2ce19b21477168c7a962
stephbsb <stephany0303@gmail.com>
2020-09-28 21:31:16 -0300

    Criação: entidade item da carteira

---
+++ src/main/java/com/wallet/entity/WalletItem.java
@@ -0,0 +1,38 @@
+package com.wallet.entity;
+
+import com.wallet.util.enums.TypeEnum;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Data
+@Table(name = "wallet_item")
+@NoArgsConstructor
+@AllArgsConstructor
+public class WalletItem implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @JoinColumn(name="wallet",referencedColumnName = "id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Wallet wallet;
+    @NotNull
+    private Date date;
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private TypeEnum type;
+    @NotNull
+    private String description;
+    @NotNull
+    private BigDecimal value;
+
+
+}
