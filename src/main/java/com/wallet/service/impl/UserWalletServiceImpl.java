d6d1e3059b3b9a1932a635edf3376fc00f778b50
stephbsb <stephany0303@gmail.com>
2020-09-28 21:29:55 -0300

    Correção: adição de anotação service

--- src/main/java/com/wallet/service/impl/UserWalletServiceImpl.java
+++ src/main/java/com/wallet/service/impl/UserWalletServiceImpl.java
@@ -4,7 +4,9 @@ import com.wallet.entity.UserWallet;
 import com.wallet.repository.UserWalletRepository;
 import com.wallet.service.UserWalletService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserWalletServiceImpl implements UserWalletService {
 
     @Autowired
--- src/main/java/com/wallet/service/impl/WalletServiceImpl.java
+++ src/main/java/com/wallet/service/impl/WalletServiceImpl.java
@@ -4,7 +4,9 @@ import com.wallet.entity.Wallet;
 import com.wallet.repository.WalletRepository;
 import com.wallet.service.WalletService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class WalletServiceImpl implements WalletService {
 
     @Autowired
